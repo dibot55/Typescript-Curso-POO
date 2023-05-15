972b2e93f61e9d7ff57c4dbe80bb9e905c579084
dibot55 <diebots555@gmail.com>
2023-05-15 13:43:26 -0600

    Herencia

---
+++ src/08-herencia.ts
@@ -0,0 +1,53 @@
+// Herencia - extender clases
+
+export class Animal {
+  constructor(public nombre: string) {
+
+  }
+
+  public comer(){
+    console.log('Comiendo');
+  };
+
+  public moving(){
+    console.log("Moviendose");
+  };
+
+  public saludar(){
+    return `Saludos, Soy ${this.nombre}`
+  };
+
+};
+
+// Instancia
+const dog = new Animal("Hachi");
+console.log(dog.saludar());
+dog.comer();
+
+// Herencia - extends
+export class Perro extends Animal {
+  // Extendiendo Atributos
+  constructor(
+    nombre:string, // <-- NO de le define un alcance por que estariamos doblando el atributo
+    public especie: string
+    ) {
+    super(nombre); // Pasamos el atributo "nombre" al constructor de la clase padre
+    };
+
+  // Extendiendo Metodos
+  public ladrar(times: number){
+    for (let index = 0; index < times; index++){
+      console.log("woof");
+    };
+  };
+};
+
+// Instancia
+const pug = new Perro("Pelusa", "Pug");
+// Metodos de la clase padre
+console.log(pug.saludar());
+pug.comer();
+pug.moving();
+// Metodos de la clase hija
+pug.ladrar(5);
+console.log(pug.especie);
