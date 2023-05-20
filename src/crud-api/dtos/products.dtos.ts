ed9c4dbdde8d21e81a04dfa216e4af383f301757
dibot55 <diebots555@gmail.com>
2023-05-20 00:19:15 -0600

    decoradaores

--- src/crud-api/dtos/products.dtos.ts
+++ src/crud-api/dtos/products.dtos.ts
@@ -1,15 +1,41 @@
 // Dtos - Objetos de transferencia de datos.
 
 import { Product } from "../models/product.models";
-import { Category } from "../models/category.model";
+import { Category, AccessType } from "../models/category.model";
+import { IsEnum, IsUrl, validate, validateOrReject } from "class-validator";
 
+// Products
 // Omit - Omite los campos seleccionados
 export interface CreatedProductDto extends Omit<Product, "id" | "category"> {
   categoryId: Category["id"]; // Acceder al tipado por un indice
 };
-export interface CreateCategoryDto extends Omit<Category, "id">{};
 
 // Partial - Vuelve opcional a todos las propiedades de la interfaz
 export interface UpdateProductDto extends Partial<CreatedProductDto> {};
+
+// Category
+export interface CreateCategoryDto extends Omit<Category, "id">{};
+  // Decoradores
+class CategoryDto implements CreateCategoryDto{
+  name!: string;
+  @IsUrl()
+  image!: string;
+  @IsEnum(AccessType)
+  access?: AccessType | undefined;
+};
+(async()=>{
+  try {
+    const dto = new CategoryDto();
+    dto.name = "a",
+    dto.image = "b", // No es una URL ERROR
+    dto.access = AccessType.PUBLIC
+
+    // Ejecutar los decoradores
+    await validateOrReject(dto);
+
+  } catch (error) {
+    console.log(error);
+  };
+})();
 
 export interface UpdateCategoryDto extends Partial<CreateCategoryDto>{};
---
+++ src/genericos/22-decoradores.ts
@@ -0,0 +1,41 @@
+// Decoradores - Funciones que agregan validaciones o comportamientos extra a las clases, metodos, propiedades y parametros de funciones. Se ejecutan al momento de ejecutar una clase o metodo que esten decorando.
+
+// Usamos Class-Validator: Mayuscula para Decorador y Minuscula para Funcion
+import {
+  validate,
+  validateOrReject,
+  Contains,
+  IsInt,
+  Length,
+  IsEmail,
+  IsFQDN,
+  IsDate,
+  Min,
+  Max,
+} from 'class-validator';
+
+
+export class Post {
+  // Por defecto pide que inicialices los atributos en el constructor - usa ! para decirle a TS que lo inicializaras en otro momento
+  // Da errores en los decoradores por que tienen datos indefinidos los atributos.
+  // Los decoradores por defecto vienen desabilitados. Activalos en tu tsconfig.json
+  @Length(10, 20)
+  title!: string;
+
+  @Contains('hello')
+  text!: string;
+
+  @IsInt()
+  @Min(0)
+  @Max(10)
+  rating!: number;
+
+  @IsEmail()
+  email!: string;
+
+  @IsFQDN()
+  site!: string;
+
+  @IsDate()
+  createDate!: Date;
+}
